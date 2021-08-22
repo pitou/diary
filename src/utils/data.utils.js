3502a973fe61598f6f2354539a447bb2ab154e34
Vito Latrofa <vito.lat@gmail.com>
2021-08-22 13:06:30 +0200

    Fix index var name and remove it from the returned object (not useful)

--- src/utils/data.utils.js
+++ src/utils/data.utils.js
@@ -1,11 +1,11 @@
 export const getPagesData = (data) => {
-  return Object.values(data).reduce((acc, dd, dayIndex) => {
+  return Object.values(data).reduce((acc, dd, pageIndex) => {
     Object.entries(dd).forEach(([key, val]) => {
       if (/\d{4}\/\d{2}\/\d{2}/.test(key) && val !== null) {
-        if (!acc[dayIndex]) {
-          acc[dayIndex] = []
+        if (!acc[pageIndex]) {
+          acc[pageIndex] = []
         }
-        acc[dayIndex].push({ date: key, val, dayIndex })
+        acc[pageIndex].push({ date: key, val })
       }
     })
     return acc
